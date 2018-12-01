6fbefae49bcfb5b9736fbe849c2862af0ab5bc91
Nina <nina_rai@hotmail.co.uk>
2018-12-01 11:55:57 +0000

    Day 1 solution

--- 2018/Day-1.py
+++ 2018/Day-1.py
@@ -1,1 +1,28 @@
-"I'M READY"
+#def finalSum(input):
+input = open("Inputs/Day-1.txt")
+s = []
+for line in input:
+    #if line.find("\n") != -1:
+        #line = input.readline()#.split('\n')[0]
+    #print(line)
+    s.append(int(line))
+print(sum(s))
+
+freq = 0
+track = []
+for n in s:
+    freq += n
+    s.append(n)
+    if freq not in track:
+        track.append(freq)
+    else:
+        break
+print(freq)
+
+
+
+
+
+#print(finalSum(open("Inp uts/Day-1.txt")))
+#print(twice(open("Inputs/Day-1.txt")))
+#print(twice())
